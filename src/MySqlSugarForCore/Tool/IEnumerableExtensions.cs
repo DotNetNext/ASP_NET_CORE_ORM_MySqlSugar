ceafe50499820f71c1181b64cc2041c4ae0c3a90
sunkaixuan <610262374@qq.com>
2016-10-20 19:11:03 +0800

    init

---
+++ src/MySqlSugarForCore/Tool/IEnumerableExtensions.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+using System.Data;
+using System.Data.SqlTypes;
+
+namespace MySqlSugar
+{
+    /// <summary>
+    /// ** 描述：IEnumerable扩展类
+    /// ** 创始时间：2015-6-9
+    /// ** 修改时间：-
+    /// ** 作者：sunkaixuan
+    /// ** 使用说明：
+    /// </summary>
+    public static class IEnumerableExtensions
+    {
+
+        static Type _guidType = typeof(Guid);
+
+        #region 单组
+        /// <summary>
+        /// 排序
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="list"></param>
+        /// <param name="sortField"></param>
+        /// <param name="orderByType"></param>
+        /// <returns></returns>
+        public static IOrderedEnumerable<T> OrderBy<T>(this IEnumerable<T> list, string sortField, OrderByType orderByType)
+        {
+            var type = typeof(T);
+            PropertyInfo prop = type.GetProperty(sortField);
+            Check.Exception(prop == null, "No property '" + sortField + "' in + " + typeof(T).Name + "'");
+            if (orderByType == OrderByType.desc)
+                return list.OrderByDescending(it => ConvertField(prop.GetValue(it, null)));
+            else
+                return list.OrderBy(it => ConvertField(prop.GetValue(it, null)));
+
+
+        }
+        /// <summary>
+        /// 排序
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="list"></param>
+        /// <param name="sortField"></param>
+        /// <param name="orderByType"></param>
+        /// <returns></returns>
+        public static IOrderedEnumerable<T> ThenBy<T>(this IOrderedEnumerable<T> list, string sortField, OrderByType orderByType)
+        {
+            var type = typeof(T);
+            PropertyInfo prop = type.GetProperty(sortField);
+            Check.Exception(prop == null, "No property '" + sortField + "' in + " + typeof(T).Name + "'");
+            if (orderByType == OrderByType.desc)
+                return list.ThenByDescending(it => ConvertField(prop.GetValue(it, null)));
+            else
+                return list.ThenBy(it => ConvertField(prop.GetValue(it, null)));
+
+        }
+
+        /// <summary>
+        /// 排序
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="list"></param>
+        /// <param name="sortField"></param>
+        /// <param name="orderByType"></param>
+        /// <returns></returns>
+        public static IOrderedEnumerable<T> OrderByDataRow<T>(this IEnumerable<T> list, string sortField, OrderByType orderByType) where T : DataRow
+        {
+            var type = typeof(T);
+            PropertyInfo prop = type.GetProperty(sortField);
+            if (orderByType == OrderByType.desc)
+                return list.OrderByDescending(it => ConvertField(it[sortField]));
+            else
+                return list.OrderBy(it => ConvertField(it[sortField]));
+
+        }
+
+        /// <summary>
+        /// 排序
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="list"></param>
+        /// <param name="sortField"></param>
+        /// <param name="orderByType"></param>
+        /// <returns></returns>
+        public static IOrderedEnumerable<T> ThenByDataRow<T>(this IOrderedEnumerable<T> list, string sortField, OrderByType orderByType) where T : DataRow
+        {
+            var type = typeof(T);
+            PropertyInfo prop = type.GetProperty(sortField);
+            if (orderByType == OrderByType.desc)
+                return list.ThenByDescending(it => ConvertField(it[sortField]));
+            else
+                return list.ThenBy(it => ConvertField(it[sortField]));
+        }
+        #endregion
+
+        /// <summary>
+        /// 解决GUID在SQL和C#中，排序方式不一致
+        /// </summary>
+        /// <param name="thisValue"></param>
+        /// <returns></returns>
+        private static object ConvertField(object thisValue)
+        {
+            if (thisValue == null) return null;
+            var isGuid = thisValue.GetType() == _guidType;
+            if (isGuid)
+            {
+                return SqlGuid.Parse(thisValue.ToString());
+            }
+            else
+            {
+                return thisValue;
+            }
+        }
+    }
+}
---
+++ src/MySqlSugarForCore/Tool/JsonConverter.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data;
+using System.Linq;
+using System.Collections;
+using System.Text.RegularExpressions;
+using Newtonsoft.Json;
+
+namespace MySqlSugar
+{
+    /// <summary>
+    /// 作者：sunkaixaun
+    /// </summary>
+    public class JsonConverter
+    {
+        /// <summary>
+        /// dataTable转成JSON
+        /// add sunkaixuan 
+        /// </summary>
+        /// <param name="table"></param>
+        /// <returns></returns>
+        public static string DataTableToJson(DataTable table)
+        {
+            List<Dictionary<string, object>> parentRow = new List<Dictionary<string, object>>();
+            foreach (DataRow row in table.Rows)
+            {
+                Dictionary<string, object> childRow = new Dictionary<string, object>();
+                foreach (DataColumn col in table.Columns)
+                {
+                    var columnValue = row[col.ColumnName];
+                    if (columnValue == DBNull.Value)
+                    {
+                        columnValue = null;
+                    }
+                    childRow.Add(col.ColumnName, columnValue);
+                }
+                parentRow.Add(childRow);
+            }
+            return JsonConvert.SerializeObject(parentRow);
+        }
+        /// <summary>
+        /// DataTable转成Json字符串
+        /// </summary>
+        /// <param name="table"></param>
+        /// <param name="dateFormat"></param>
+        /// <returns></returns>
+        public static string DataTableToJson(DataTable table, string dateFormat)
+        {
+            var reval = DataTableToJson(table);
+            if (dateFormat.IsNullOrEmpty()) return reval;
+            reval = Regex.Replace(reval, @"\\/Date\((\d+)\)\\/", match =>
+            {
+                DateTime dt = new DateTime(1970, 1, 1);
+                dt = dt.AddMilliseconds(long.Parse(match.Groups[1].Value));
+                dt = dt.ToLocalTime();
+                return dt.ToString(dateFormat);
+            });
+            return reval;
+        }
+
+
+        /// <summary>
+        /// json转换object动态类
+        /// add duk by 2016-05-19
+        /// </summary>
+        /// <param name="json"></param>
+        /// <returns></returns>
+        public static dynamic ConvertJson(string json)
+        {
+            var dy = JsonConvert.DeserializeObject<dynamic>(json);
+            return dy;
+        }
+
+        /// <summary>
+        /// 将对象序列化成字符串
+        /// </summary>
+        /// <param name="o"></param>
+        /// <returns></returns>
+        public static string Serialize(object o)
+        {
+            return JsonConvert.SerializeObject(o);
+        }
+    }
+}
